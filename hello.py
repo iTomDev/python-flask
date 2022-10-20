ec35ac194642ff40ccff5cfdbdcb21e5491ad502
Thomas Pile <Thomas.Pile@gmail.com>
2022-10-21 00:02:00 +0100

    sqlite add data

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy as sp
 from scipy.spatial import Delaunay
 import matplotlib.pyplot as plt
+import sqlite3
 
 def hello():
     return "Hello World!"
@@ -19,3 +20,20 @@ def spatialDemo():
     plt.triplot(points[:.0], points[:,1], simplices)
     plt.scatter(points[:,0], points[:,1], color='r')
     plt.show()
+    
+def sqlDemoInit():
+    conn = sqlite3.connect("test.db")
+    conn.execute('''CREATE TABLE COMPANY
+         (ID INT PRIMARY KEY     NOT NULL,
+         NAME           TEXT    NOT NULL,
+         AGE            INT     NOT NULL,
+         ADDRESS        CHAR(50),
+         SALARY         REAL);''')
+    conn.close()
+    
+def sqlDemoAddData():
+    conn = sqlite3.connect('test.db')
+    conn.execute("INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) \ VALUES (1, 'Paul', 32, 'California', 20000.00 )");
+    conn.commit()
+    conn.close()
+    
--- tests.py
+++ tests.py
@@ -7,3 +7,6 @@
 def test_sum(self):
     assert self.simpleSum(1,2) == 3
     #assert sum([1,2,3]) == 6, "should be 6"
+
+# def test_sqlite3_demo(self):
+    
