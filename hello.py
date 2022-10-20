35a2f82ba03e08a7f9789e9e54aa6e64e6a6492b
Thomas Pile <Thomas.Pile@gmail.com>
2022-10-20 16:27:11 +0100

    spatial_demo

--- hello.py
+++ hello.py
@@ -1,6 +1,19 @@
 import numpy as np
 import scipy as sp
+from scipy.spatial import Delaunay
+import matplotlib.pyplot as plt
 
 def hello():
     return "Hello World!"
     
+def spatial_demo():
+    points = np.array([
+        [2,4],
+        [3,4],
+        [3,0],
+        [2,2],
+        [4,1]])
+    simplices = Delaunay(points).simplices
+    plt.triplot(points[:.0], points[:,1], simplices)
+    plt.scatter(points[:,0], points[:,1], color='r')
+    plt.show()
